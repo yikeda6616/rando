8e99291de20f7897dc9bfed16e46699ef7947357
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-23 20:29:30 -0700

    Build

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,26 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = __importStar(require("express"));
-const path = __importStar(require("path"));
-const generatePassword = __importStar(require("password-generator"));
-const app = express();
-const PORT = process.env.PORT || 5000;
+exports.__esModule = true;
+var express = require("express");
+var path = require("path");
+var generatePassword = require("password-generator");
+var app = express();
+var PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/api/passwords', (req, res) => {
-    const count = 5;
+app.get('/api/passwords', function (req, res) {
+    var count = 5;
     // Generate some passwords
-    const passwords = Array.from(Array(count).keys()).map(i => generatePassword(12, false));
+    var passwords = Array.from(Array(count).keys()).map(function (i) {
+        return generatePassword(12, false);
+    });
     res.json(passwords);
-    console.log(`Sent ${count} passwords`);
+    console.log("Sent " + count + " passwords");
 });
 /*
  * The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
  *
  */
-app.get('*', (req, res) => {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 app.listen(PORT);
-console.log(`Password generator listening on ${PORT}`);
+console.log("Password generator listening on " + PORT);
